7ba0f8e64289d1627b16d8d82e2cd3dde3bcc84b
ykamara-appc <ykamara@appcelerator.com>
2014-04-25 14:17:42 +0530

    Push Notifications Screen

--- app/controllers/pushnotifications.js
+++ app/controllers/pushnotifications.js
@@ -5,12 +5,94 @@ function initialize(){
 	$.topBar.back.addEventListener('click',closeWindow);
 	$.topBar.setTitle('Push Notifications');
 }
+var acs = require("/acs");
+
+/**
+ * EVENT LISTENER
+ */
+
+/**
+ * Subscribe/Unsubscribe channels for sending push notifications
+ */
+function enablePush() {
+		//http://docs.appcelerator.com/titanium/latest/#!#!/guide/Push_Notifications
+		if(OS_IOS){
+			Ti.Network.registerForPushNotifications({
+			    // Specifies which notifications to receive
+			    types: [
+			        Ti.Network.NOTIFICATION_TYPE_BADGE,
+			        Ti.Network.NOTIFICATION_TYPE_ALERT,
+			        Ti.Network.NOTIFICATION_TYPE_SOUND
+			    ],
+			    success: deviceTokenSuccess,
+			    error: deviceTokenError,
+			    callback: receivePush
+			});
+			// Process incoming push notifications
+			
+		} else {
+			//http://docs.appcelerator.com/titanium/latest/#!/api/Titanium.CloudPush
+			var CloudPush = require('ti.cloudpush');
+			
+			CloudPush.enabled = true;
+			
+			CloudPush.retrieveDeviceToken({
+			    success: deviceTokenSuccess,
+			    error: deviceTokenError
+			});
+			
+			CloudPush.addEventListener('callback', receivePush);
+			
+			CloudPush.addEventListener('trayClickLaunchedApp', function (evt) {
+			    Ti.API.info('Tray Click Launched App (app was not running)');
+			});
+			
+			CloudPush.addEventListener('trayClickFocusedApp', function (evt) {
+			    Ti.API.info('Tray Click Focused App (app was already running)');
+			});
+			
+		}
+	}
+	/**
+	 * Unsubscribe from push channels
+	 */
+	function unsubscribe(){
+		acs.unsubscribe({channel:"main", token:Ti.App.Properties.getString("deviceToken")});
+	    acs.unsubscribe({channel:Ti.Platform.osname, token:Ti.App.Properties.getString("deviceToken")});
+	    $.enablePushBtn.title = "Subscribe to Channels";
+		$.enablePushBtn.removeEventListener("click",unsubscribe);
+		$.enablePushBtn.addEventListener("click",enablePush);
+	}
+	
+	function receivePush(e) {
+	    alert('Received push: ' + JSON.stringify(e));
+	}
+	
+	/**
+	 * Success Callback
+	 */
+	function deviceTokenSuccess(e) {
+		
+		Ti.App.Properties.setString("deviceToken", e.deviceToken);
+	    acs.subscribe({channel:"main", token:e.deviceToken});
+	    acs.subscribe({channel:Ti.Platform.osname, token:e.deviceToken});
+	    $.enablePushBtn.title = "Unsubscribe from Channels";
+		$.enablePushBtn.removeEventListener("click",enablePush);
+		$.enablePushBtn.addEventListener("click",unsubscribe);
+	}
+	
+	function deviceTokenError(e) {
+	    alert('Failed to register for push notifications! ' + e.error);
+	}
+
+
+
 
 /**
  * Closes the window 
  * */
 function closeWindow(){
-	(OS_IOS)?Alloy.Globals.navGroup.closeWindow($.pushnotoficationWin): $.pushnotoficationWin.close();
+	(OS_IOS)?Alloy.Globals.navGroup.closeWindow($.pushnotificationWin): $.pushnotificationWin.close();
 }
 
 initialize();
