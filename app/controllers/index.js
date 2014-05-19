28f2f5d8b9e3d5ff28ec99e2ca234cda22d213e7
Sapan Varshney <svarshney@appcelerator.com>
2014-05-19 15:59:10 +0530

    Created menu view using views instead of tableviews. Tableview row doesn't supports multiple platforms in views.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -14,8 +14,18 @@ function initialize() {
 	//Add click event listener to open/close menu
 	main.iconContainer.addEventListener('click', $.drawermenu.showhidemenu);
 
-	//Add click event listener to open/close menu
-	menu.container.addEventListener('click', openChildWindow);
+	//For blackberry platform, source returns the container on which the event is attached
+	//whereas for ios/android it returns the object which is clicked, hence attaching the 
+	//events for every menu item rahther then to the container for BB platform.
+	if (OS_IOS || OS_ANDROID) {
+		//Add click event listener to menuItem Container
+		menu.container.addEventListener('click', openChildWindow);
+	} else if (OS_BLACKBERRY) {
+		var childrens = menu.container.children;
+		for (var i=0;i<childrens.length;i++) {
+			childrens[i].addEventListener('click', openChildWindow);
+		}
+	}
 
 	// Add view to main view container exposed by widget
 	$.drawermenu.drawermainview.add(main.getView());
@@ -30,12 +40,15 @@ function initialize() {
  * Opens the child window when menu item is clicked
  * */
 function openChildWindow(e) {
-	var menuClicked = (e.row.controller) ? e.row.controller :  e.row.id;
-	if (e.row.id === 'home') {
+	var source = e.source;
+	var menuClicked = (source.controller) ? source.controller : source.id;
+	if (source.id === 'home') {
 		$.drawermenu.showhidemenu();
 	} else {
-		if (e.row.id !== 'container') {
-			var childWindow = Alloy.createController(menuClicked,{data: e.row}).getView();
+		if (source.id !== 'container') {
+			var childWindow = Alloy.createController(menuClicked, {
+				data : source
+			}).getView();
 			childWindow.open();
 		}
 	}
@@ -45,5 +58,4 @@ initialize();
 
 //Open main window
 $.mainWin.open();
-
 
--- app/controllers/menu.js
+++ app/controllers/menu.js
@@ -1,3 +1,20 @@
 //Sets the top for ios>=7
-Alloy.Globals.adjustStatusBar($.tableContainer);
+Alloy.Globals.adjustStatusBar($.container);
 
+/**
+ * This changes the background color for menu rows
+ * */
+function changeBackgroundColor(e) {
+	var source = e.source;
+	if (source.id !=='container') {
+		var initialBackgroundColor = source.backgroundColor;
+		// set new BackgroundColor
+		source.backgroundColor = '#555';
+
+		// wait 500 milliseconds and switch back to the original BackgroundColor
+		setTimeout(function() {
+			source.backgroundColor = initialBackgroundColor;
+		}, 500);
+	}
+
+}
