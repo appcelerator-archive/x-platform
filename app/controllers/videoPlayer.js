27a50058a7fc5d9fc2826973288ad36f1a799a69
ykamara-appc <ykamara@appcelerator.com>
2014-05-20 15:22:49 +0530

    Video Player feature on iOS and Android
    Localization in progress

---
+++ app/controllers/videoPlayer.js
@@ -0,0 +1,50 @@
+var loading  =  Ti.UI.createActivityIndicator();
+/**
+ * Screen Initialization
+ * */
+function initialize() {
+	$.topBar.imageContainer.addEventListener('click', closeWindow);
+	$.topBar.setTitle("Video Player");
+	$.movieView.add(loading);
+}
+
+/**
+ * Event Listeners
+ */
+
+$.movie.addEventListener('load', function(){
+	loading.hide();
+});
+
+
+/**
+ * Plays the local video
+ */
+function playLocal(e){
+	$.movie.url = '/movie.mp4';
+	$.movie.play();
+}
+
+/**
+ * Plays the remote video
+ */
+function playRemote(e){
+	var contentURL = 'http://movies.apple.com/media/us/ipad/2010/tours/apple-ipad-video-us-20100127_r848-9cie.mov';
+	if (OS_ANDROID) {
+		loading.show();
+		contentURL = "http://dts.podtrac.com/redirect.mp4/twit.cachefly.net/video/aaa/aaa0033/aaa0033_h264b_640x368_256.mp4";
+	}
+	$.movie.url = contentURL;
+	$.movie.play();
+}
+
+
+/**
+ * Closes the window
+ * */
+function closeWindow() {
+	$.movie.stop();
+	$.videoPlayerWin.close();
+}
+
+initialize();
