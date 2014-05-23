f168d70ccfd311ae57280059153d8ecf501832a6
ykamara-appc <ykamara@appcelerator.com>
2014-05-23 12:39:33 +0530

    fixing first name last name length issue
    for iOS, Android and Blackberry

--- app/controllers/partials/dbRow.js
+++ app/controllers/partials/dbRow.js
@@ -1,6 +1,7 @@
 var args = arguments[0] || {};
 
-$.rowTitle.text = args.first_name + " " + args.last_name;
+$.firstName.text = args.first_name;
+$.lastName.text = args.last_name;
 $.localDbRow.fname = args.first_name;
 $.localDbRow.lname = args.last_name;
 $.localDbRow.index = args.id;
