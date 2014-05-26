d79aae2a279390145faa03f8ac92fd8eddedbe9a
Sapan Varshney <svarshney@appcelerator.com>
2014-05-26 17:50:19 +0530

    Added UI fro model screen for IOS

--- app/controllers/model.js
+++ app/controllers/model.js
@@ -4,13 +4,21 @@
 function initialize() {
 	$.topBar.imageContainer.addEventListener('click', closeWindow);
 	$.topBar.setTitle(L('model'));
-}
+};
 
 /**
  * Closes the window
  * */
 function closeWindow() {
 	$.modelWin.close();
-}
+};
+
+
+/**
+ * Add the book
+ * */
+function addBook(){
+	
+};
 
 initialize();
