4b6f4df7d263717ba37c5968d4bdd08133711436
Sapan Varshney <sapan.varshney@appcelerator.com>
2014-05-27 17:13:13 +0530

    Added add/update/delete book functionality via models for IOS platform

--- app/controllers/model.js
+++ app/controllers/model.js
@@ -1,9 +1,13 @@
+var books = Alloy.Collections.book;
+var bookId = "";
 /**
  * Screen Initialization
  * */
 function initialize() {
 	$.topBar.imageContainer.addEventListener('click', closeWindow);
 	$.topBar.setTitle(L('model'));
+	//Fetches all the data
+	books.fetch();
 };
 
 /**
@@ -13,12 +17,116 @@ function closeWindow() {
 	$.modelWin.close();
 };
 
-
 /**
- * Add the book
+ * Add the book in DB
  * */
-function addBook(){
-	
+function addBook() {
+	// Create a new model for the todo collection
+	var book = Alloy.createModel('book', {
+		title : $.titleVal.value,
+		author : $.authorVal.value
+	});
+	// save the model to persistent storage, which will give
+	// a uniqueid and update the UI
+	if (book.isValid() > 0) {
+		// add new model to the global collection
+		books.add(book, {
+			silent : true
+		});
+		book.save();
+		clearData();
+	}
 };
 
+/**
+ * Update the book in DB
+ * */
+function updateBook() {
+	var book = Alloy.createModel('book', {
+		title : $.titleVal.value,
+		author : $.authorVal.value
+
+	});
+	if (book.isValid() > 0) {
+		// Performs a GET on /book/id
+		book.fetch({
+			id : bookId
+		});
+
+		// Performs a PUT on /book/id with the entire modified object as a payload.
+		book.save({
+			checkout : true,
+			title : $.titleVal.value,
+			author : $.authorVal.value
+
+		});
+		books.fetch();
+		clearData();
+	}
+
+}
+
+/**
+ * Delete the book from DB
+ * */
+function deleteBook(id) {
+	var book = Alloy.createModel('book');
+	// Performs a GET on /book/id
+	book.fetch({
+		id : id
+	});
+	// Performs a DELETE on /book/id
+	book.destroy();
+	books.fetch();
+	clearData();
+}
+
+function performOperation(e) {
+	var id = e.source.rowid;
+	switch(e.source.type) {
+		case "edit":
+			$.add.title = L("update_book");
+			$.titleVal.value = e.source.title;
+			$.authorVal.value = e.source.author;
+			bookId = id;
+			break;
+		case "delete":
+			var dialog = Ti.UI.createAlertDialog({
+				message : L('delRecord'),
+				ok : L("ok"),
+				buttonNames : [L("ok"), L('cancel')],
+				cancel : 1
+			});
+			dialog.show();
+			dialog.addEventListener("click", function(e) {
+				if (e.index === 0) {
+					deleteBook(id);
+				}
+			});
+			break;
+	}
+}
+
+/**
+ * Perform cleanup tasks
+ * */
+function clearData() {
+	bookId = "";
+	$.titleVal.value = "";
+	$.authorVal.value = "";
+	$.add.title = L("add_book");
+}
+
+/**
+ * Decide whether to add/upadte the data based of bookid
+ * */
+function addupdate(e) {
+	if (bookId === "") {
+		addBook();
+	} else {
+		updateBook();
+	}
+}
+
+//Initializes Screen
 initialize();
---
+++ app/models/book.js
@@ -0,0 +1,46 @@
+//http://docs.appcelerator.com/titanium/latest/#!/guide/Alloy_Collection_and_Model_Objects
+exports.definition = {
+	config : {
+		columns : {
+			"id" : "integer primary key autoincrement",
+			"title" : "text",
+			"author" : "text"
+		},
+		adapter : {
+			type : "sql",
+			collection_name : "book",
+			idAttribute : "id"
+		}
+	},
+	extendModel : function(Model) {
+		_.extend(Model.prototype, {
+			validate : function(attrs) {
+				for (var key in attrs) {
+					var value = attrs[key];
+					if (key === "title") {
+						if (value.trim().length <= 0) {
+							return "Error: No title!";
+						}
+					}
+					if (key === "author") {
+						if (value.trim().length <= 0) {
+							return "Error: No author!";
+						}
+					}
+				}
+			}
+		});
+		
+        return Model;
+	},
+	extendCollection : function(Collection) {
+		_.extend(Collection.prototype, {
+			// Implement the comparator method.
+    	    comparator : function(book) {
+        	    return book.get('title');
+            }
+		});
+
+		return Collection;
+	}
+}; 
