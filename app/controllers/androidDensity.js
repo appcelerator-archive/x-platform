fa314dee3d7121f0bfff046eb9e40525e50aa06a
Alan Leard <aleard@appcelerator.com>
2014-06-03 16:26:54 -0700

    Added screen size to controller display

--- app/controllers/androidDensity.js
+++ app/controllers/androidDensity.js
@@ -4,10 +4,8 @@
 function initialize() {
 	$.topBar.imageContainer.addEventListener('click', closeWindow);
 	$.topBar.setTitle( L('androidDensity'));
-
+	$.info.text=+"\n"+Ti.Platform.displayCaps.platformWidth+" x "+Ti.Platform.displayCaps.platformHeight;
 }
-
-
 
 /**
  * Closes the window
