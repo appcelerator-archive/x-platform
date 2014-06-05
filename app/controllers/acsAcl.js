87d1105dddad370ab07de2393559fc40ef5a139d
Veer Jain <vjain@appcelerator.com>
2014-06-05 12:04:10 +0530

    ACL works for android

--- app/controllers/acsAcl.js
+++ app/controllers/acsAcl.js
@@ -47,19 +47,24 @@ function selectUser(evt) {
 	}
 }
 
-function animateView(e){
-	var source= $[e.source.id+'View'];
+function animateView(e) {
+	var source = $[e.source.id + 'View'];
 	source.visible = true;
 	var height = source.height === 1 ? Ti.UI.SIZE : 1;
-	var animationObject = Ti.UI.createAnimation({
-		height : height,
-		duration : 200
-	});
-	animationObject.addEventListener('complete', function(){
+	if (OS_IOS) {
+		var animationObject = Ti.UI.createAnimation({
+			height : height,
+			duration : 200
+		});
+		animationObject.addEventListener('complete', function() {
+			source.height = height;
+			source.visible = source.height === 1 ? false : true;
+		});
+		source.animate(animationObject);
+	} else {
 		source.height = height;
 		source.visible = source.height === 1 ? false : true;
-	});
-	source.animate(animationObject);
+	}
 }
 
 function selectReader() {
@@ -187,20 +192,23 @@ function queryUsers() {
 			} else {
 				var data = [];
 				if ( typeof access.publicAccess != 'undefined') {
-					data.push({
+					var rowData = {
 						title : '<Public Access>',
 						id : 'publicAccess',
 						hasCheck : access.publicAccess || false
-					});
+					};
+					var row = Alloy.createController('partials/aclRow',{data: rowData});
+					data.push(row.getView());
 				}
 				for (var i = 0, l = e.users.length; i < l; i++) {
 					var user = e.users[i];
-					var row = Ti.UI.createTableViewRow({
+					var rowData = {
 						title : user.first_name + ' ' + user.last_name,
-						id : user.id
-					});
-					row.hasCheck = access.ids.indexOf(user.id) != -1;
-					data.push(row);
+						id : user.id,
+						hasCheck : (access.ids.indexOf(user.id) != -1)
+					};
+					var row = Alloy.createController('partials/aclRow',{data: rowData});
+					data.push(row.getView());
 				}
 				$.userTable.setData(data);
 				$.tableContent.visible = true;
