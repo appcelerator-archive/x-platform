012b0e77671510cd95242ac4bda031b75c7dcc23
Alan Leard <aleard@appcelerator.com>
2014-06-06 10:38:10 -0700

    Added all ACS examples in once menu item from ti.cloud module example

--- app/alloy.js
+++ app/alloy.js
@@ -53,3 +53,117 @@ Alloy.Globals.db = "Employee";
 if(OS_IOS || OS_ANDROID || OS_BLACKBERRY){
 	Alloy.Collections.book = Alloy.createCollection('book');
 }
+
+var Cloud = require('ti.cloud');
+var pushToken = '';
+Cloud.debug = true;
+
+// Find out if this is iOS 7 or greater
+function isIOS7Plus() {
+    if (Titanium.Platform.name == 'iPhone OS') {
+        var version = Titanium.Platform.version.split(".");
+        var major = parseInt(version[0],10);
+        // can only test this support on a 3.2+ device
+        if (major >= 7) {
+            return true;
+        }
+    }
+    return false;
+
+}
+var IOS7 = isIOS7Plus();
+var top = IOS7 ? 20 : 0;
+
+// Define our window store.
+
+
+
+// Utility functions for defining windows.
+u = Ti.Android != undefined ? 'dp' : 0;
+
+windowFunctions = {};
+
+createWindow = function() {
+    return Ti.UI.createWindow({
+        backgroundColor: '#fff',
+        navBarHidden: true
+    });
+};
+
+handleOpenWindow = function(evt) {
+    var target = (evt.row && evt.row.title) || evt.target;
+    if (windowFunctions[target]) {
+        windowFunctions[target](evt);
+    }
+};
+
+
+addBackButton = function (win) {
+    if (Ti.Android) {
+        return 0;
+    }
+    var back = Ti.UI.createButton({
+        title: 'Back',
+        color: '#fff', backgroundColor: '#000',
+        style: 0,
+        top: top, left: 0, right: 0,
+        height: 40 + u
+    });
+    back.addEventListener('click', function (evt) {
+        win.close();
+    });
+    win.add(back);
+    return 40 + top;
+};
+
+createRows = function(rows) {
+    for (var i = 0, l = rows.length; i < l; i++) {
+        rows[i] = Ti.UI.createTableViewRow({
+            backgroundColor: '#fff',
+            title: rows[i],
+            hasChild: true,
+            height: 30 + u,
+            font: { fontSize: 20 + u }
+        });
+    }
+    return rows;
+};
+
+enumerateProperties = function(container, obj, offset) {
+    for (var key in obj) {
+        if (!obj.hasOwnProperty(key))
+            continue;
+        container.add(Ti.UI.createLabel({
+            text: key + ': ' + obj[key], textAlign: 'left',
+            color: '#000', backgroundColor: '#fff',
+            height: 30 + u, left: offset, right: 20 + u
+        }));
+        if (obj[key].indexOf && obj[key].indexOf('http') >= 0 
+            && (obj[key].indexOf('.jpeg') > 0 || obj[key].indexOf('.jpg') > 0 || obj[key].indexOf('.png') > 0)) {
+            container.add(Ti.UI.createImageView({
+                image: obj[key],
+                height: 120 + u, width: 120 + u,
+                left: offset
+            }));
+        }
+        if (typeof(obj[key]) == 'object') {
+            enumerateProperties(container, obj[key], offset + 20);
+        }
+    }
+};
+
+error = function(e) {
+    var msg = (e.error && e.message) || JSON.stringify(e);
+    if (e.code) {
+        alert(msg);
+    } else {
+        Ti.API.error(msg);
+    }
+};
+
+convertISOToDate = function(isoDate) {
+    isoDate = isoDate.replace(/\D/g," ");
+    var dtcomps = isoDate.split(" ");
+    dtcomps[1]--;
+    return new Date(Date.UTC(dtcomps[0],dtcomps[1],dtcomps[2],dtcomps[3],dtcomps[4],dtcomps[5]));
+};
---
+++ app/controllers/acsExamples.js
@@ -0,0 +1,87 @@
+var args = arguments[0] || {};
+/**
+ * Screen Initialization
+ * */
+function initialize() {
+	$.topBar.imageContainer.addEventListener('click', closeWindow);
+	$.topBar.setTitle(L('acs_examples'));
+}
+
+/**
+ * Closes the window
+ * */
+function closeWindow() {
+	$.acsExamples.close();
+}
+
+initialize() 
+/*
+ * We'll follow a really simple paradigm in this example app. It's going to be a hierarchy of tables where you can drill
+ * in to individual examples for each ACS namespace.
+ *
+ * To facilitate that, we will have a collection of "windowFunctions" like the "Users" window, and the "Login" window.
+ *
+ * These are defined in the "windows" folder and its children.
+ *
+ * That's it! Enjoy.
+ */
+
+// Include the window hierarchy.
+Ti.include(
+    '/acsExamples/windows/chats/table.js',
+    '/acsExamples/windows/checkins/table.js',
+    '/acsExamples/windows/clients/table.js',
+    '/acsExamples/windows/customObjects/table.js',
+    '/acsExamples/windows/emails/table.js',
+    '/acsExamples/windows/events/table.js',
+    '/acsExamples/windows/files/table.js',
+    '/acsExamples/windows/friends/table.js',
+    '/acsExamples/windows/geoFences/table.js',
+    '/acsExamples/windows/photoCollections/table.js',
+    '/acsExamples/windows/photos/table.js',
+    '/acsExamples/windows/places/table.js',
+    '/acsExamples/windows/posts/table.js',
+    '/acsExamples/windows/keyValues/table.js',
+    '/acsExamples/windows/likes/table.js',
+    '/acsExamples/windows/messages/table.js',
+    '/acsExamples/windows/pushNotifications/table.js',
+    '/acsExamples/windows/pushSchedules/table.js',
+    '/acsExamples/windows/reviews/table.js',
+    '/acsExamples/windows/social/table.js',
+    '/acsExamples/windows/status/table.js',
+    '/acsExamples/windows/users/table.js',
+    '/acsExamples/windows/accessControlLists/table.js'
+);
+
+
+var table = Ti.UI.createTableView({
+    backgroundColor: '#fff',
+    data: createRows([
+        'Users',
+        'Access Control Lists',
+        'Chats',
+        'Checkins',
+        'Clients',
+        'Custom Objects',
+        'Emails',
+        'Events',
+        'Files',
+        'Friends',
+        'GeoFences',
+        'Key Values',
+        'Likes',
+        'Messages',
+        'Photo Collections',
+        'Photos',
+        'Places',
+        'Posts',
+        'Push Notifications',
+        'Push Schedules',
+        'Reviews',
+        'Social',
+        'Status'
+    ])
+});
+table.addEventListener('click', handleOpenWindow);
+$.win.add(table);
+$.win.open();
