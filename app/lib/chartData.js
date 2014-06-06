9458fac7c74747432f83ae2e922a78349338894a
Alan Leard <aleard@appcelerator.com>
2014-06-06 10:41:33 -0700

    Updated charts

--- app/lib/chartData.js
+++ app/lib/chartData.js
@@ -550,6 +550,68 @@ module.exports = [
 	        data: [[47,47,21],[20,12,4],[6,76,91],[38,30,60],[57,98,64],[61,17,80],[83,60,13],[67,78,75],[64,12,10],[30,77,82]]
 	    }]
 	
-	}
+	},
+	{
+        title: {
+            text: 'Combination chart'
+        },
+        xAxis: {
+            categories: ['Apples', 'Oranges', 'Pears', 'Bananas', 'Plums']
+        },
+        labels: {
+            items: [{
+                html: 'Total fruit consumption',
+                style: {
+                    left: '50px',
+                    top: '18px',
+                    color:  'black'
+                }
+            }]
+        },
+        series: [{
+            type: 'column',
+            name: 'Jane',
+            data: [3, 2, 1, 3, 4]
+        }, {
+            type: 'column',
+            name: 'John',
+            data: [2, 3, 5, 7, 6]
+        }, {
+            type: 'column',
+            name: 'Joe',
+            data: [4, 3, 3, 9, 0]
+        }, {
+            type: 'spline',
+            name: 'Average',
+            data: [3, 2.67, 3, 6.33, 3.33],
+            marker: {
+            	lineWidth: 2,
+            	lineColor: "blue",
+            	fillColor: 'white'
+            }
+        }, {
+            type: 'pie',
+            name: 'Total consumption',
+            data: [{
+                name: 'Jane',
+                y: 13,
+                color: "red" // Jane's color
+            }, {
+                name: 'John',
+                y: 23,
+                color: "green" // John's color
+            }, {
+                name: 'Joe',
+                y: 19,
+                color: "purple" // Joe's color
+            }],
+            center: [100, 80],
+            size: 100,
+            showInLegend: false,
+            dataLabels: {
+                enabled: false
+            }
+        }]
+    }
 	
 ];
